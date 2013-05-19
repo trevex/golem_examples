ac1446fe41ba4f3441fb86fd9eabab6121e234bd
Nik Voss <niklas.voss@gmail.com>
2013-05-19 21:12:13 +0100

    Commented example and renamed it.

--- example_data.go
+++ example_data.go
@@ -10,22 +10,37 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 
+// This struct represent the message the json function
+// should receive. If a function takes a special data
+// type that is not an byte array, golem automatically
+// tries to unmarshal the data in to this specific type.
+// Since the json package is used tags work as well.
 type ChatMessage struct {
 	Msg string `json:"msg"`
 }
 
+// Function taken special data type and utilizing golem's
+// inbuilt unmarshalling
 func json(conn *golem.Connection, data *ChatMessage) {
 	fmt.Println("JSON:  ", data.Msg)
 }
 
+// If a function accepts a byte array the data is directly
+// forwarded to the function without any parsing involved.
+// Hence it is the fastest way.
 func raw(conn *golem.Connection, data []byte) {
 	fmt.Println("Raw:   ", string(data))
 }
 
+// If a parser is known for the specific data type it is
+// automatically used.
 func custom(conn *golem.Connection, data string) {
 	fmt.Println("Custom:", data)
 }
 
+// Custom parsers take a byte array as argument and return
+// the data type they parse to and a boolean (to validate if
+// parsing was successful).
 func customParser(data []byte) (string, bool) {
 	return string(data), true
 }
@@ -33,16 +48,22 @@ func customParser(data []byte) (string, bool) {
 func main() {
 	flag.Parse()
 
+	// Add the custom parser that returns strings
 	golem.AddParser(customParser)
 
+	// Create a router
 	myrouter := golem.NewRouter()
+	// Add the events to the router
 	myrouter.On("json", json)
 	myrouter.On("raw", raw)
 	myrouter.On("custom", custom)
 
+	// Serve the public files
 	http.Handle("/", http.FileServer(http.Dir("./public")))
+	// Handle websockets using golems handler
 	http.HandleFunc("/ws", myrouter.Handler())
 
+	// Listen
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
