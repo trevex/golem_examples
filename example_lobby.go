f686bb6103767334684102e58ae3b68453869e97
Nik Voss <niklas.voss@gmail.com>
2013-05-20 14:53:42 +0100

    Lobby example started.

---
+++ example_lobby.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/trevex/golem"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+var hub = golem.GetHub()
+
+type JoinMessage struct {
+	Name string `json:"name"`
+}
+
+func join(conn *golem.Connection, data *JoinMessage) {
+	hub.JoinLobby(conn, data.Name)
+	fmt.Println("Joined ", data.Name)
+}
+
+func main() {
+	flag.Parse()
+
+	// Create a router
+	myrouter := golem.NewRouter()
+	// Add the events to the router
+	myrouter.On("join", join)
+
+	// Serve the public files
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	// Handle websockets using golems handler
+	http.HandleFunc("/ws", myrouter.Handler())
+
+	// Listen
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
