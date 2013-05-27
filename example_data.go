ec2f54b42d843a7f93318db71185597e35db93c0
Nik Voss <niklas.voss@gmail.com>
2013-05-27 10:50:18 +0200

    Simple data example updated to use Emit instead of channel that is not public anymore.

--- example_data.go
+++ example_data.go
@@ -23,7 +23,7 @@ type ChatMessage struct {
 // inbuilt unmarshalling
 func json(conn *golem.Connection, data *ChatMessage) {
 	fmt.Println("JSON:  ", data.Msg)
-	conn.Send <- []byte("Json received.")
+	conn.Emit("json", data)
 }
 
 // If a function accepts a byte array the data is directly
@@ -31,14 +31,14 @@ func json(conn *golem.Connection, data *ChatMessage) {
 // Hence it is the fastest way.
 func raw(conn *golem.Connection, data []byte) {
 	fmt.Println("Raw:   ", string(data))
-	conn.Send <- []byte("Raw byte array received.")
+	conn.Emit("raw", []byte("Raw byte array received."))
 }
 
 // If a parser is known for the specific data type it is
 // automatically used.
 func custom(conn *golem.Connection, data string) {
 	fmt.Println("Custom:", data)
-	conn.Send <- []byte("Custom handler use to receive data.")
+	conn.Emit("custom", "Custom handler use to receive data.")
 }
 
 // Custom parsers take a byte array as argument and return
