33b7b622e0d047e090a3f0fd69c829f4a2d28d15
Nik Voss <niklas.voss@gmail.com>
2013-06-05 11:43:52 +0200

    Improved documentation of BSON protocol example.

--- example_protocol.go
+++ example_protocol.go
@@ -17,16 +17,20 @@ var addr = flag.String("addr", ":8080", "http service address")
 
 type BSONProtocol struct{}
 
+// RawIncomingBSONMessage represents the first step of unmarshalling of incoming data
+// and partially unmarshals the incoming data to get the event name.
 type RawIncomingBSONMessage struct {
 	Event string   `bson:"e"`
 	Data  bson.Raw `bson:"d,omitempty"`
 }
 
+// General structure to represent outgoing data.
 type OutgoingBSONMessage struct {
 	Event string      `bson:"e"`
 	Data  interface{} `bson:"d,omitempty"`
 }
 
+// Partially unmarshal incoming data to unpack event name.
 func (_ *BSONProtocol) Unpack(data []byte) (string, []byte, error) {
 	rawMsg := &RawIncomingBSONMessage{}
 	err := bson.Unmarshal(data, rawMsg)
@@ -36,6 +40,7 @@ func (_ *BSONProtocol) Unpack(data []byte) (string, []byte, error) {
 	return rawMsg.Event, rawMsg.Data.Data, nil
 }
 
+// Unmarshal the leftover data into the desired type of the callback.
 func (_ *BSONProtocol) Unmarshal(data []byte, structPtr interface{}) error {
 	raw := bson.Raw{
 		Kind: 3, // Embedded document
@@ -44,6 +49,7 @@ func (_ *BSONProtocol) Unmarshal(data []byte, structPtr interface{}) error {
 	return raw.Unmarshal(structPtr)
 }
 
+// Marshal and pack data into array of bytes for sending.
 func (_ *BSONProtocol) MarshalAndPack(name string, structPtr interface{}) ([]byte, error) {
 	outMsg := &OutgoingBSONMessage{
 		Event: name,
@@ -52,16 +58,19 @@ func (_ *BSONProtocol) MarshalAndPack(name string, structPtr interface{}) ([]byt
 	return bson.Marshal(outMsg)
 }
 
+// Read mode should be binary for BSON
 func (_ *BSONProtocol) GetReadMode() int {
 	return golem.BinaryMode
 }
 
+// Write mode should be binary for BSON
 func (_ *BSONProtocol) GetWriteMode() int {
 	return golem.BinaryMode
 }
 
 /*
  * Message types
+ * See example_simple for more documentation.
  */
 
 type Hello struct {
@@ -75,6 +84,8 @@ type Answer struct {
 
 /*
  * Event handlers
+ * See example_simple for more documentation on these handlers.
+ * This program is the same except the different protocol in use.
  */
 
 func hello(conn *golem.Connection, data *Hello) {
@@ -92,8 +103,13 @@ func main() {
 
 	// Create a router
 	myrouter := golem.NewRouter()
+
+	// Create protocol instance representing the interface
 	protocol := &BSONProtocol{}
+	// Use the new protocol, if all future handlers should use the protocol
+	// golem.SetInitialProtocol can be used.
 	myrouter.SetProtocol(protocol)
+
 	// Add the events to the router
 	myrouter.On("hello", hello)
 	myrouter.On("poke", poke)
